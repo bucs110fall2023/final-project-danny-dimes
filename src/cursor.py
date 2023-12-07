366e3939cdba976292044729e5b0c059931a2d86
MichaelIcar <micarangal@binghamton.edu>
2023-12-07 10:49:22 -0500

    c

--- src/cursor.py
+++ src/cursor.py
@@ -3,13 +3,13 @@ import pygame
 class Cursor(pygame.sprite.Sprite):
     """
     This class represents the cursor used in the Duck Hunt game. It is responsible
-    for tracking the mouse position andS detecting mouse clicks
+    for tracking the mouse position and detecting mouse clicks
     The cursor sprite is updated to follow the mouse movements.
     Args: None
     Returns: None
     """
     clicked = False
-    
+    pygame.mouse.set_visible(False)
     xPos = pygame.mouse.get_pos()[0]
     yPos = pygame.mouse.get_pos()[1]
     
@@ -18,20 +18,17 @@ class Cursor(pygame.sprite.Sprite):
         super(Cursor, self).__init__()
         
         # Load image and rect attributes
-        self.image = pygame.image.load("Sprites/cursor.png")
+        self.image = pygame.image.load("assets/crosshairs.png") 
         self.rect = self.image.get_rect()
         self.rect.topleft = (Cursor.xPos, Cursor.yPos)
-        
         self.mouseClicked = False
-        self.mouseCounter = 0
-
-    
         
+ 
     def update(self):
         # Keep the sprite at the same x and y location as the mouse
         self.rect.topleft = (Cursor.xPos, Cursor.yPos)
 
-    def tick(self, is_paused, is_over, total_shots, elevate_foreground):
+    def tick(self, is_paused, is_over, total_shots):
         """ Check For Mouse Click """
         if not is_paused and not is_over:
             # Check if the mouse was clicked
@@ -40,17 +37,7 @@ class Cursor(pygame.sprite.Sprite):
                 Cursor.clicked = True
               
                 total_shots += 1
-            
-            # Avoid repeated mouse clicks 
-            if Cursor.clicked:
-                if self.mouseCounter > 10 and not pygame.mouse.get_pressed()[0]:
-                    Cursor.clicked = False
-                    self.mouseCounter = 0
-                else:
-                    self.mouseCounter += 1
-
+        
             # Update cursor position
             Cursor.xPos, Cursor.yPos = pygame.mouse.get_pos()
 
-            # Bring the tree and grass in front of all the ducks
-            elevate_foreground()
--- src/game.py
+++ src/game.py
@@ -14,11 +14,7 @@ class Game(pygame.sprite.Sprite):
     """
     image = pygame.image.load("Sprites\spawner.png")
 
-    # Scoring
-    score = 0
-    ducks_hit = 0
-    total_shots = 0  # Total Shots Taken
-    total_ducks = 0  # Total Ducks Spawned
+    
 
     # State of Game
     paused = False  # True when game is paused
@@ -35,29 +31,9 @@ class Game(pygame.sprite.Sprite):
     def __init__(self):
         super(Game, self).__init__()
 
-        # Instructions Labels
-        self.instructions = pygame.font.Font(None, 35).render(
-            "Shoot as many ducks as possible in 1 minute!", True, (255, 255, 255)
-        )
-        self.instructions_rect = self.instructions.get_rect(x=320, y=100)
+        
 
-        self.instructions2 = pygame.font.Font(None, 35).render(
-            "Press \"P\" To Pause", True, (255, 255, 255)
-        )
-        self.instructions2_rect = self.instructions2.get_rect(x=320, y=140)
-
-        # Paused Game Sprite
-        self.paused = pygame.sprite.Sprite()
-        self.paused.image = pygame.image.load("Sprites/paused.png")  #change
-        self.paused.rect = self.paused.image.get_rect(center=(320, 240))
-        self.paused.dx = 0
-        self.paused.dy = 0
-
-        # Final Results Labels
-        self.results = pygame.font.Font(None, 35).render("", True, (255, 255, 255))
-        self.results_rect = self.results.get_rect(x=320, y=100)  # How many ducks were hit
-        self.results2 = pygame.font.Font(None, 35).render("", True, (255, 255, 255))
-        self.results2_rect = self.results2.get_rect(x=320, y=140)  # Accuracy
+       
 
         # Counters to delay events
         self.spawn_counter = 0
@@ -84,24 +60,6 @@ class Game(pygame.sprite.Sprite):
         self.add(new_duck)
 
     def update(self):
-        # Check if the game time is up
-        if Game.over:
-            self.menu_counter = 0
-            self.playing = False
-            Game.over = True
-
-            # Show results
-            self.results.value = (
-                "You hit " + str(Game.ducks_hit) + " of " + str(Game.total_ducks) + " ducks!"
-            )
-            self.results2.value = "Accuracy: " + str(
-                int((int(Game.ducks_hit) / Game.total_shots) * 100)
-            ) + "%"
-
-            self.add(self.results)
-            self.add(self.results2)
-
-            self.kill()
 
     def tick(self):
         if self.playing and not Game.paused and not Game.over:
