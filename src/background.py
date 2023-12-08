9c9166559f9703c0675b778765c06677a793bb6e
Markus Massina <mmassina@binghamton.edu>
2023-12-07 22:52:27 -0500

    update

--- src/background.py
+++ src/background.py
@@ -1,9 +1,10 @@
 import pygame
+
 class Background(pygame.sprite.Sprite):
 
     def __init__(self, image_file= 'assets/background.png', location=[0,0]):
         super().__init__()
-        self.screen = pygame.display.set_mode()
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.width, self.height = pygame.display.get_window_size()
         self.image = pygame.image.load(image_file)
         self.image = pygame.transform.scale(self.image, (self.width, self.height))
